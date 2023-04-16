f7410bdb5045d948d84aa85ec09e62f08b093a42
Z Zhang <zuoning@AndrewZs-MacBook-Air.local>
2023-04-16 14:31:52 -0700

    fix

--- gpg.py
+++ gpg.py
@@ -16,13 +16,15 @@ with open('files.txt', 'r') as file:
         # Run the commands and connect the output of cmd1 to the input of cmd2 using pipes
         p1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
         p2 = subprocess.Popen(cmd2, stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, error = p2.communicate()
-        # Decode the output and error messages from bytes to string
-        output = output.decode()
-        print(output)
+        p1.stdout.close()
+        p1.wait()
+        p2.wait()
+        output_messages_stdout = p2.stdout.read().decode()
+        output_messages_stderr = p2.stderr.read().decode()
         output_file = "output.txt"
         with open(output_file, "a") as outfile:
             # Write the text to a new line
-            outfile.write(f"{output}\n")
+            outfile.write(f"{output_messages_stdout}\n")
+            outfile.write(f"{output_messages_stderr}\n")
             outfile.write(f"i:{i}")
         i+=1
