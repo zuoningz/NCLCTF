31e85075481cbc69c35bd2a85ba33852152d09f9
Z Zhang <zuoning@AndrewZs-MacBook-Air.local>
2023-04-16 14:36:53 -0700

    fix

--- gpg.py
+++ gpg.py
@@ -1,12 +1,6 @@
 import subprocess
 
 # Directory path to loop through
-cmd1 = ["cat", "files.txt"]
-cmd2 = ["gpg", "--verify-files"]
-
-# Run the commands and connect the output of cmd1 to the input of cmd2 using pipes
-p1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
-p2 = subprocess.Popen(cmd2, stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 with open('files.txt', 'r') as file:
     i = 0
     # Loop through each line in the file
@@ -14,17 +8,31 @@ with open('files.txt', 'r') as file:
         # Process the line
         print(line.strip())
         filename = line.strip()
-        # cmd = ["gpg", "--verify", filename]
-        # output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode()
+        # Define the command with pipes
+        cmd1 = ["cat", filename]
+        cmd2 = ["gpg", "--verify-files"]
+
+        # Run the commands and connect the output of cmd1 to the input of cmd2 using pipes
+        p1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(cmd2, stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=b'')  # Pass an empty input
         p1.stdout.close()
         p1.wait()
         p2.wait()
+
+        # Get the exit code of the gpg command
+        exit_code = p2.returncode
+
+        # Get the output messages from stdout and stderr
         output_messages_stdout = p2.stdout.read().decode()
         output_messages_stderr = p2.stderr.read().decode()
+
         output_file = "output.txt"
         with open(output_file, "a") as outfile:
             # Write the text to a new line
-            outfile.write(f"{output_messages_stdout}\n")
-            outfile.write(f"{output_messages_stderr}\n")
-            outfile.write(f"i:{i}")
-        i+=1
+            outfile.write(f"File: {filename}\n")
+            outfile.write(f"Exit code: {exit_code}\n")
+            outfile.write(f"Stdout messages:\n{output_messages_stdout}\n")
+            outfile.write(f"Stderr messages:\n{output_messages_stderr}\n")
+            outfile.write(f"i: {i}\n")
+            outfile.write("\n")
+        i += 1
