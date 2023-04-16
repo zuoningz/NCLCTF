35202996e79b32093fd627603d1d14396c749d96
Z Zhang <zuoning@AndrewZs-MacBook-Air.local>
2023-04-16 14:19:45 -0700

    fix

--- gpg.py
+++ gpg.py
@@ -8,8 +8,17 @@ with open('files.txt', 'r') as file:
         # Process the line
         print(line.strip())
         filename = line.strip()
-        cmd = ["gpg", "--verify", filename]
-        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode()
+        # cmd = ["gpg", "--verify", filename]
+        # output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode()
+        cmd1 = ["cat", "files.txt"]
+        cmd2 = ["gpg", "--verify-files"]
+
+        # Run the commands and connect the output of cmd1 to the input of cmd2 using pipes
+        p1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(cmd2, stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = p2.communicate()
+        # Decode the output and error messages from bytes to string
+        output = output.decode()
         output_file = "output.txt"
         with open(output_file, "a") as outfile:
             # Write the text to a new line
